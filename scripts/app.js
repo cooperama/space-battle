076d0fc0879a4d257d84321080f3f607874d263e
cooperama <coope133@gmail.com>
2020-09-18 08:37:37 -0700

    Commit 4 - fixes infinite loop & initial styling

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,14 @@ console.log('Space Battle')
 console.log('------*-------*-------*------')
 
 
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
+
+
+
+const begin = document.getElementById('begin');
 
 
 // ------------------------------------------------------
@@ -19,14 +27,14 @@ class Spaceship {
     this.accuracy = accuracy;
   }
   attack(target) {
+    alert('Pew pew!')
     console.log(`${this.name} shoots and...`)
+
     if (Math.random() < this.accuracy) {
       target.hull -= this.firepower;
-      console.log(`hits ${target.name}!`);
-      // return target.stillAlive();                  //////???????
-      // target.displayStats();
+      console.log(`                       %c  hits ${target.name}!`, 'color: #bf2c2c');
     } else {
-      console.log(`missed its target!`);
+      console.log(`                       %c  misses its target!`, 'color: #6ba1e8');
     }
   }
   getHull() {
@@ -34,23 +42,19 @@ class Spaceship {
   }
   displayStats() {
       console.log(`
-      ---------------
-      -${this.name}-
-      --------------
-      hull: ${this.hull}
-      fire power: ${this.firepower}
-      ---------------`);
+                ---------------
+                -${this.name}-
+                --------------
+                hull: ${this.hull}
+                fire power: ${this.firepower}
+                ---------------`);
   }
 }
+
 
 class MySpaceship extends Spaceship {
   constructor(name, hull=20, firepower=5, accuracy=0.7) {
     super(name, hull, firepower, accuracy);
-  }
-
-  retreat() {
-    console.log('RETREAT!');
-    game.playing = false;
   }
 
   stillAlive() {
@@ -60,7 +64,7 @@ class MySpaceship extends Spaceship {
          -${this.name}-
          --------------
          hull: ${this.hull}
-         ---------------
+         --------------- %c
          ▓██   ██▓ ▒█████   █    ██     ██▓     ▒█████    ██████ ▓█████ 
          ▒██  ██▒▒██▒  ██▒ ██  ▓██▒   ▓██▒    ▒██▒  ██▒▒██    ▒ ▓█   ▀ 
           ▒██ ██░▒██░  ██▒▓██  ▒██░   ▒██░    ▒██░  ██▒░ ▓██▄   ▒███   
@@ -71,25 +75,31 @@ class MySpaceship extends Spaceship {
          ▒ ▒ ░░  ░ ░ ░ ▒   ░░░ ░ ░      ░ ░   ░ ░ ░ ▒  ░  ░  ░     ░   
          ░ ░         ░ ░     ░            ░  ░    ░ ░        ░     ░  ░
          ░ ░                                                              
-      `)
+      `, 'color: #bf2c2c');
       return false;
     }
     return true;
   }
 }
 
+
 class AlienSpaceship extends Spaceship {
-  constructor(name, hull, firepower, accuracy) {
+  constructor(
+    name, 
+    hull = Math.floor(Math.random() * (6-3) + 3), 
+    firepower = Math.floor(Math.random() * (4-2) + 2), 
+    accuracy = Math.random() * (.8-.6) + .6
+    ) {
     super(name, hull, firepower, accuracy);
   }
 
   stillAlive() {
     if (this.getHull() <= 0) {
       console.log(`
-      ---------------
-      -${this.name}-
-        -DESTROYED-
-      --------------
+                ---------------
+                -${this.name}-
+                  -DESTROYED-
+                --------------
       `)
       
       return false;
@@ -104,32 +114,25 @@ class AlienSpaceshipFactory {
     this.alienShips = [];
   }
   generateShip(num=1) {
-    let hull;
-    let firepower;
-    let accuracy;
     let name;
     let newShip;
 
     for (let i = 0; i < num; i++) {
 
-      hull = Math.floor(Math.random() * (6-3) + 3) + 1;
-      firepower = Math.floor(Math.random() * (4-2) + 2) + 1;
-      accuracy = Math.random() * (.8-.6) + .6;
       name = `Alien Ship ${this.alienShips.length + 1}`;
-
-      newShip = new AlienSpaceship(name, hull, firepower, accuracy)
+      newShip = new AlienSpaceship(name)
       this.alienShips.push(newShip);
     }
   }
   displayHorde() {
     this.alienShips.forEach(ship => {
       console.log(`
-      ---------------
-      -${ship.name}-
-      --------------
-      hull: ${ship.hull}
-      fire power: ${ship.firepower}
-      ---------------`);
+                ---------------
+                -${ship.name}-
+                --------------
+                hull: ${ship.hull}
+                fire power: ${ship.firepower}
+                ---------------`);
     })
     return this.alienShips;
   }
@@ -150,26 +153,72 @@ class AlienSpaceshipFactory {
 // GAME PLAY
 const game = {
   playing: false,
+  hull: 20,
+  firepower: 5,
+  accuracy: 0.7,
+  youWin: false,
+  retreat: false,
   intro() {
     console.log(`
     Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship.
 
     Battle the aliens as you try to destroy them with your lasers.
+    `);
+    console.log(`
+    The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. 
+    
+    Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. 
+    `);
+    console.log(`
+    After you have destroyed a ship, you have the option to make a hasty retreat.
 
-    There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.
-    `)
+    If you choose to retreat, your hull will be reinforced with 5 extra points, however, your accuracy will decrease by 10%. If your accuracy reaches 0, you lose.
+    `);
   },
   start() {
-    let youWin = false;
-    this.intro();
+    youWin = false;
+    retreat = false;
     playing = true;
-
-    const USS_Assembly = new MySpaceship('USS Assembly');   // generate players
+    
+    const USS_Assembly = new MySpaceship('USS Assembly', this.hull, this.firepower, this.accuracy);   // generate players
+    
     const alienHorde = new AlienSpaceshipFactory();
-    alienHorde.generateShip(6);
-
-    // const alienShip = alienHorde.alienShips[0];
+    
+    let randomNumber = Math.floor(Math.random() * (15 - 6) + 6) + 1;
+    alienHorde.generateShip(randomNumber);
     const alienShips = alienHorde.alienShips;
+    
+    this.intro();
+
+    prompt('Are you ready?');
+
+    console.log(`%c
+    ********************************
+              ********************************
+                        ********************************
+                                  ********************************
+                                            ********************************
+                                  ********************************
+                        ********************************
+              ********************************
+    ********************************
+    `, 'color: #773fab')
+
+    alienHorde.displayHorde();
+
+    prompt('Let\'s do it!')
+
+    console.log(`%c
+                                            ********************************
+                                  ********************************
+                        ********************************
+              ********************************
+    ********************************
+              ********************************
+                        ********************************
+                                  ********************************
+                                            ********************************
+    `, 'color: #2d611c')
 
     while (playing) {
 
@@ -184,21 +233,20 @@ const game = {
 
           // prompt for player choice when one ship has been destroyed
           let attackAgain = prompt(`
-             You destroyed one ship!
-          Attack next ship or retreat?
-                   attack: a
-                   retreat: r
+               You destroyed a ship!      
+                     What now?            
+                attack (a)  ||  retreat (r)
           `)
-          if (attackAgain.toLowerCase() !== 'a') {
-            game.playing = false;
-            game.youWin = false;
+          if (attackAgain.toLowerCase() === 'r') {
+            this.playing = false;
+            this.youWin = false;
+            retreat = true;
             break;
           }
 
-
           alienHorde.removeFromHorde();
           if (!alienHorde.isHordeAlive()) {
-            youWin = true;
+            this.youWin = true;
             this.playing = false;
             break;
           }
@@ -206,31 +254,37 @@ const game = {
 
       } else {
         this.playing = false;
+        break;
       }
-
     }
-    this.winner(youWin);
+    if (!this.retreat) {
+      this.winner(this.youWin);
+    } else {
+      this.retreatRestart();
+    }
   },
   winner(win) {
     if (win) {
-      console.log(`
-        ***********************************
-                   ***********************************
-  ____    ____  ______    __    __     ____    __    ____  __  .__   __. 
-  \   \  /   / /  __  \  |  |  |  |    \   \  /  \  /   / |  | |  \ |  | 
-   \   \/   / |  |  |  | |  |  |  |     \   \/    \/   /  |  | |   \|  | 
-    \_    _/  |  |  |  | |  |  |  |      \            /   |  | |  . '  | 
-      |  |    |  '--'  | |  '--'  |       \    /\    /    |  | |  |\   | 
-      |__|     \______/   \______/         \__/  \__/     |__| |__| \__| 
-                                                                                                                  
-                   ***********************************
-        ***********************************
-   `)
+      console.log(`%c
+                                          *********************************************
+   *********************************************
+      ---      ---   --------   ----    ----      ---      --- --------  ----    ---- 
+      ***    ***   **********  ****    ****      ***  **  *** ********  *****   **** 
+       ---  ---   ----    ---- ----    ----      ---  --  ---   ----    ------  ---- 
+        ******    ***      *** ****    ****      ***  **  ***   ****    ************ 
+         ----     ---      --- ----    ----      ---  --  ---   ----    ------------ 
+         ****     ****    **** ************      ************   ****    ****  ****** 
+         ----      ----------  ------------       ----------  --------  ----   ----- 
+         ****       ********   ************        ********   ********  ****    **** 
+                                                                                     
+   *********************************************
+                                          *********************************************
+      `, 'color: #5482f7')
     } else {
-      console.log(`
-                            ***********************************
-      ***********************************
-      ▄████  ▄▄▄       ███▄ ▄███▓▓█████     ▒█████   ██▒   █▓▓█████  ██▀███  
+      console.log(`%c
+                                        *****************************************
+  *****************************************
+       ▄████  ▄▄▄       ███▄ ▄███▓▓█████     ▒█████   ██▒   █▓▓█████  ██▀███  
       ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀    ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒
      ▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███      ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒
      ░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄    ▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  
@@ -240,12 +294,35 @@ const game = {
      ░ ░   ░   ░   ▒   ░      ░      ░      ░ ░ ░ ▒       ░░     ░     ░░   ░ 
            ░       ░  ░       ░      ░  ░       ░ ░        ░     ░  ░   ░     
                                                           ░                   
-      ***********************************
-                            ***********************************
-      `)
+  *****************************************
+                                        *****************************************
+      `, 'color: #bf2c2c')
+    }
+    let startOver = prompt(`
+    ***********************************
+                      ***********************************
+                Play Again?    ----    y / n
+                      ***********************************
+    ***********************************
+    `);
+    if (startOver.toLowerCase() === 'y') {
+      if (this.youWin ) {
+        this.start();
+      } else {
+        this.retreatRestart();
+      }
+  }
+  },
+  retreatRestart() {
+    this.hull += 5;
+    this.accuracy -= 0.1;
+    if (this.accuracy <= 0) {
+      this.winner(false);
+    } else {
+      this.start()
     }
   }
 }
 
 
-game.start();
+// game.start();
